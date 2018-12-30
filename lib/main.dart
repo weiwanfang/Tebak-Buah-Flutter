444bb63c4a48e6600ab2a1cb6d6d32b322729f7c
Muhibbudin Suretno <muhibbudinsuretno1@gmail.com>
2018-12-30 23:08:13 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      routes: {
+        "/": (_) => new WebviewScaffold(
+          url: "https://titiarimba.github.io/Tebak-Buah-Game/"
+        ),
+      },
+    );
+  }
+}
